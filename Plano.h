9cfcc5b6110d80e0a90c82ece553fbc301428226
Uiseman <42385063+Uiseman@users.noreply.github.com>
2019-06-09 18:11:27 -0300

    Add files via upload

--- Plano.h
+++ Plano.h
@@ -1,126 +1,38 @@
 #ifndef PLANO_H
+
 #define PLANO_H
 
+#include <string>
 
-#include<iostream>
 
-#include "Date.h"
-#include "PrePago.h"
-#include "PosPago.h"
-#include "Exception.h"
 
-using namespace std;
+#include "Data.h"
 
-class Plano{
+ class Plano {
 
-private:
-    string nome;
-    double valorMinuto;
-    double velocidade;
-    double franquia;
-    double velocAlem;
-    double credito;
-    bool tipo; //true = prePago;
-    Date validade;
-    vector <PrePago> pre;
-    vector <PosPago> pos;
-    //PrePago pre;
-    //PosPago pos;
+ public:
 
-public:
-    Plano(string planName ="Plano",double valMin = 0,double vel = 0,double fran = 0,double velAlem = 0,bool preOrPos = false,Date val = (0,0,0,0))
-       {
-        nome = planName;
-        valorMinuto = valMin;
-        velocidade= vel;
-        franquia= fran*10*10*10; //Em megabytes
-        velocAlem = velAlem;
-        tipo = preOrPos;
-        validade = val;
-        if(tipo == true){
-            nome += " PrePago";
-            cout<<"Quantos creditos serao incluidos no plano "<<nome<<"?"<<endl;
-            cin>>credito;
-            PrePago p(credito,val);
-            pre.push_back(p);
-            //pre = p;
-        }else{
-            nome += " PosPago";
-            PosPago a(val);
-            pos.push_back(a);
-            //pos  = a;
-        }
-        }
+ virtual string getNome()=0;
 
-    virtual string getNome(){return nome;}
-    double getVM(){return valorMinuto;}//centavos
-    double getVel(){return velocidade;}//Mbps
-    double getFran(){return franquia;}//Mbs
-    double getVelAlem(){return velocAlem;}//Kbps
-    double getCred(){return credito;}//centavos
-    bool getTipo(){return tipo;}
-    PrePago &getPre(){return pre[pre.size()];}
-    PosPago &getPos(){return pos[pos.size()];}
-    Date &getValidade(){return validade;}
+ virtual bool isPrepago ()=0;
 
-    bool testaValidade(Date &d){
 
-        if( (validade.getDia() >= d.getDia())&&(validade.getMes() == d.getMes())){
-            return false;//Fora da validade
-        }else{
-            return true;
-        }
+ virtual double getFran()=0;
 
-    }
+ virtual double custoDeChamada()= 0;
 
-    void setPlano(string pN,double vMin,double vel,double fran,double vAlem){
-        nome = pN;
-        valorMinuto = vMin;
-        velocidade = vel;
-        franquia = fran;
-        velocAlem = vAlem;
-    }
+ virtual ~Plano(){}
 
-    void setVencimento(const Date &d){
-        validade = d;
-    }
 
-    void setAddCreditos(double cred){
-        credito += cred;
-        validade.setAddMes(3);
-    }
 
-    void setDecreCreditos(double gasto){
-        credito -= gasto;
-        if(credito < 0){
-            throw Exception("Creditos insuficientes para realizar a ligacao");
-        }
-    }
 
-    void setDecreFran(double gasto){//Gasto em megabytes
-        franquia -= gasto;
-        if(franquia < 0){
-            throw Exception("Creditos insuficientes para realizar a ligacao");
-        }
-    }
 
-    ~Plano(){
-        nome = "--";
-        valorMinuto = 0;
-        velocidade = 0;
-        franquia = 0;
-        velocAlem = 0;
-        credito = 0;
-        tipo = false;
-        validade.~Date();
-        for(int i = 0;i< pre.size();i++){
-            pre[i].~PrePago();
-        }
-        for(int i = 0;i< pos.size();i++){
-            pos[i].~PosPago();
-        }
-    }
 
-};
+ };
+
+
+
+
+
 
 #endif
